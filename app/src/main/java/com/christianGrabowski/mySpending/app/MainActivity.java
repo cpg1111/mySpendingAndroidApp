cad037ffe37e38a30d3786017f84cd9372a2c961
Christian <christiang11754@gmail.com>
2014-06-12 23:35:45 -0400

    add phonegap parts

--- app/src/main/java/com/christianGrabowski/mySpending/app/MainActivity.java
+++ app/src/main/java/com/christianGrabowski/mySpending/app/MainActivity.java
@@ -1,38 +1,19 @@
+
 package com.christianGrabowski.mySpending.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.phonegap.api.*;
+
+import org.apache.cordova.DroidGap;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.loadUrl("file:///assets/www/mySpendingEmber/index.html");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
